acd3f677b399d9819606f3a5c4ef16b3bc773b99
Anton Lazutin <laa.63@yandex.ru>
2023-11-27 18:49:59 +0300

    created custom errors, middlewares-auth and models

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const { SECRET_KEY = 'mesto' } = process.env;
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Необходима авторизация'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, SECRET_KEY);
+  } catch (err) {
+    return next(new UnauthorizedError('Необходима авторизация'));
+  }
+
+  req.user = payload;
+  next();
+
+  return true;
+};
---
+++ models/user.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+const emailRegex = require('../utils/constants');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Это поле должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля - 2'],
+      maxlength: [30, 'Максимальная длина поля - 30'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Это поле должно быть заполнено'],
+      unique: true,
+      validate: {
+        validator(email) {
+          return emailRegex.test(email);
+        },
+        message: 'Введите корректный Email',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Это поледолжно быть заполнено'],
+      select: false,
+    },
+  },
+  { versionKey: false },
+);
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password,
+) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Нерпавильные логин или пароль');
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          throw new UnauthorizedError('Нерпавильные почта или пароль');
+        }
+
+        return user;
+      });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
